c7bc67f21e407c47ff7723f321126ab37fb7d51f
Raimund Canzler <git@rai-canzler.de>
2026-06-27 00:28:04 +0200

    feat: highlight speed crank on hover

--- ui/speed_crank_controller.gd
+++ ui/speed_crank_controller.gd
@@ -18,6 +18,17 @@ var prev_angle : float = NAN
 
 var avg_speed : float = 0
 
+func _ready() -> void:
+	self.mouse_entered.connect(_on_mouse_enter)
+	self.mouse_exited.connect(_on_mouse_exit)
+
+func _on_mouse_enter() -> void:
+	self.modulate = Color(1.1, 1.1, 1.1, 1)
+
+func _on_mouse_exit() -> void:
+	self.modulate = Color(1, 1, 1, 1)
+
+
 func _on_crank_input(event: InputEvent) -> void:
 	
 	if event is InputEventMouse:
