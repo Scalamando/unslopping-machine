cb827a0f3a6a136e1222d34fd498b8cc07f11b3f
Raimund Canzler <git@rai-canzler.de>
2026-06-27 01:58:24 +0200

    feat: debug stuff

--- scenes/main.gd
+++ scenes/main.gd
@@ -6,3 +6,9 @@ func _input(event: InputEvent) -> void:
 		get_tree().quit()
 	elif event.is_action_pressed("give_me_money") && OS.is_debug_build():
 		FinanceManager.add_money(100)
+	elif event.is_action_pressed("end_level", false, true) && OS.is_debug_build():
+		$"..".end_current_level()
+	elif event.is_action_pressed("goto_end") && OS.is_debug_build():
+		$"..".level_idx = len($"..".levels)
+		$"..".end_current_level()
+		$"..".next_level()
