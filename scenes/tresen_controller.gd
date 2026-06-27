a6e5d7fae5680162e5d1f8c8ef7f981e41dc1f6d
Rafael Wortmann <rabe.wortmann@gmail.com>
2026-06-27 15:01:33 +0200

    fix: tresen ui not showing

--- scenes/tresen_controller.gd
+++ scenes/tresen_controller.gd
@@ -1,6 +1,14 @@
 extends Area2D
 
+
 func _on_drop_zone_drop_applied(_zone: DropZone, area: Area2D, _plan: DropPlan) -> void:
+	display_cloth(area)
+
+
+func _on_drop_zone_drop_rejected(_zone: DropZone, area: Area2D, _plan: DropPlan) -> void:
+	display_cloth(area)
+
+func display_cloth(area : Area2D) -> void:
 	if (area is not Cloth):
 		return;
 
